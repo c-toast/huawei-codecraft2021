6ac97c14a4c808e867220b44cbbfe4125fc84993
ctoast <1324869251@qq.com>
2021-04-01 23:12:18 +0800

    delete unused

--- compile.sh
+++
@@ -1,7 +0,0 @@
-rm -r ./SDK_C++/CodeCraft-2021/include
-rm -r ./SDK_C++/CodeCraft-2021/src
-cp -r ./include ./SDK_C++/CodeCraft-2021
-cp -r ./src ./SDK_C++/CodeCraft-2021
-./SDK_C++/build.sh
-./SDK_C++/CodeCraft_zip.sh
-
--- utils.h
+++
@@ -1,7 +0,0 @@
-#define LOGD(args...) fprintf(stdout,"D:");fprintf(stdout, ##args); putchar('\n')
-#define LOGI(args...) fprintf(stdout,"I:");fprintf(stdout, ##args); putchar('\n')
-#define LOGE(args...) fprintf(stdout,"E:");fprintf(stdout, ##args); putchar('\n')
-
-char* readFile(char* filePath,int size);
-
-int writeFile(char* filePath,char* data);
